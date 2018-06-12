e8a8c041e644abaa17c2f87f9906e68c2f2627b1
Moritz Ulrich <moritz@tarn-vedra.de>
2018-06-12 17:38:32 +0200

    Remove unnecessary update triggers.

--- src/schema.sql
+++ src/schema.sql
@@ -5,18 +5,6 @@ create table effects (
   json TEXT NOT NULL
 );
 
-create trigger no_delete_trigger
-  before delete on effects
-  begin
-    select raise(rollback, "delete not allowed");
-  end;
-
-create trigger no_update_trigger
-  before update on effects
-  begin
-    select raise(rollback, "update not allowed");
-  end;
-
 create table numerical_ids (
   scope TEXT NOT NULL,
   id INTEGER NOT NULL,
