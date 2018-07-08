2985fb0aa8ad279dfcc2df97572c63660deced13
Moritz Ulrich <moritz@tarn-vedra.de>
2018-07-08 23:53:10 +0200

    Move command to its own submodule and extract Flag.

---
+++ src/command/flag.rs
@@ -0,0 +1,83 @@
+use task::{Tag, Task, Priority};
+
+use std::str::FromStr;
+use std::fmt;
+use regex::Regex;
+
+#[derive(PartialEq, Eq, Debug)]
+pub enum Flag {
+    Priority(Priority),
+    TagPositive(Tag),
+    TagNegative(Tag),
+}
+
+impl Flag {
+    pub fn from_str<S: AsRef<str>>(s: S) -> Option<Flag> {
+        lazy_static! {
+            static ref PRIORITY_RE: Regex = Regex::new("^p(?:riority)?:(.+)$").unwrap();
+            static ref TAG_POS_RE: Regex = Regex::new("^\\+(.+)$").unwrap();
+            static ref TAG_NEG_RE: Regex = Regex::new("^-(.+)$").unwrap();
+        }
+
+        let s = s.as_ref();
+
+        // TODO: Write a loop
+        let priority = PRIORITY_RE
+            .captures(s)
+            .and_then(|cs| cs.get(1))
+            .map(|m| m.as_str())
+            .and_then(|s| Priority::from_str(s).ok())
+            .map(Flag::Priority);
+
+        let pos_tag = TAG_POS_RE
+            .captures(s)
+            .and_then(|cs| cs.get(1))
+            .map(|m| m.as_str())
+            .map(String::from)
+            .map(Flag::TagPositive);
+
+        let neg_tag = TAG_NEG_RE
+            .captures(s)
+            .and_then(|cs| cs.get(1))
+            .map(|m| m.as_str())
+            .map(String::from)
+            .map(Flag::TagNegative);
+
+        priority.or(pos_tag).or(neg_tag)
+    }
+
+    pub fn matches(&self, t: &Task) -> bool {
+        use self::Flag::*;
+        match *self {
+            Priority(p) => t.priority == p,
+            TagPositive(ref tag) => t.tags.contains(tag),
+            TagNegative(ref tag) => !t.tags.contains(tag),
+        }
+    }
+
+    pub fn apply_to(&self, t: &mut Task) {
+        use self::Flag::*;
+        match *self {
+            Priority(p) => {
+                t.priority = p;
+            }
+            TagPositive(ref tag) => {
+                t.tags.insert(tag.clone());
+            }
+            TagNegative(ref tag) => {
+                t.tags.remove(tag);
+            }
+        }
+    }
+}
+
+impl fmt::Display for Flag {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::Flag::*;
+        match *self {
+            Priority(ref p) => f.write_fmt(format_args!("priority:{}", p)),
+            TagPositive(ref t) => f.write_fmt(format_args!("+{}", t)),
+            TagNegative(ref t) => f.write_fmt(format_args!("-{}", t)),
+        }
+    }
+}
--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,10 +1,11 @@
 use task::*;
 use task_ref::{TaskRef, TaskRefError, TaskRefs};
 
-use regex::Regex;
-use std::fmt::Debug;
 use std::str::FromStr;
 use std::{env, fmt};
+
+mod flag;
+pub use self::flag::*;
 
 // TODO: Use a proper enum
 #[derive(Debug, PartialEq, Eq, Fail)]
@@ -14,84 +15,6 @@ pub struct ParseError(pub String);
 impl From<TaskRefError> for ParseError {
     fn from(_e: TaskRefError) -> Self {
         ParseError("Failed to parse task-id".into())
-    }
-}
-
-#[derive(PartialEq, Eq, Debug)]
-pub enum Flag {
-    Priority(Priority),
-    TagPositive(Tag),
-    TagNegative(Tag),
-}
-
-impl Flag {
-    pub fn from_str<S: AsRef<str>>(s: S) -> Option<Flag> {
-        lazy_static! {
-            static ref PRIORITY_RE: Regex = Regex::new("^p(?:riority)?:(.+)$").unwrap();
-            static ref TAG_POS_RE: Regex = Regex::new("^\\+(.+)$").unwrap();
-            static ref TAG_NEG_RE: Regex = Regex::new("^-(.+)$").unwrap();
-        }
-
-        let s = s.as_ref();
-
-        // TODO: Write a loop
-        let priority = PRIORITY_RE
-            .captures(s)
-            .and_then(|cs| cs.get(1))
-            .map(|m| m.as_str())
-            .and_then(|s| Priority::from_str(s).ok())
-            .map(Flag::Priority);
-
-        let pos_tag = TAG_POS_RE
-            .captures(s)
-            .and_then(|cs| cs.get(1))
-            .map(|m| m.as_str())
-            .map(String::from)
-            .map(Flag::TagPositive);
-
-        let neg_tag = TAG_NEG_RE
-            .captures(s)
-            .and_then(|cs| cs.get(1))
-            .map(|m| m.as_str())
-            .map(String::from)
-            .map(Flag::TagNegative);
-
-        priority.or(pos_tag).or(neg_tag)
-    }
-
-    pub fn matches(&self, t: &Task) -> bool {
-        use self::Flag::*;
-        match *self {
-            Priority(p) => t.priority == p,
-            TagPositive(ref tag) => t.tags.contains(tag),
-            TagNegative(ref tag) => !t.tags.contains(tag),
-        }
-    }
-
-    pub fn apply_to(&self, t: &mut Task) {
-        use self::Flag::*;
-        match *self {
-            Priority(p) => {
-                t.priority = p;
-            }
-            TagPositive(ref tag) => {
-                t.tags.insert(tag.clone());
-            }
-            TagNegative(ref tag) => {
-                t.tags.remove(tag);
-            }
-        }
-    }
-}
-
-impl fmt::Display for Flag {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use self::Flag::*;
-        match *self {
-            Priority(ref p) => f.write_fmt(format_args!("priority:{}", p)),
-            TagPositive(ref t) => f.write_fmt(format_args!("+{}", t)),
-            TagNegative(ref t) => f.write_fmt(format_args!("-{}", t)),
-        }
     }
 }
 
@@ -119,7 +42,7 @@ impl Command {
         Self::from_slice(&args)
     }
 
-    fn from_slice<S: Debug + AsRef<str>>(args: &[S]) -> Result<Self, ParseError> {
+    fn from_slice<S: fmt::Debug + AsRef<str>>(args: &[S]) -> Result<Self, ParseError> {
         let task_refs = args.iter()
             .take_while(|s| TaskRef::from_str(s.as_ref()).is_ok())
             .map(|s| TaskRef::from_str(s.as_ref()).unwrap())
